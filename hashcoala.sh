cba41ad348abb525e4fb4dd0b49129d24b42e9d8
coalinha12 <gabrielstellato120@gmail.com>
2016-08-27 02:58:27 +0000

    Add files via upload

---
+++ hashcoala.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+
+echo -n "Digite uma palavra para converter: "
+read palavra
+echo ""
+
+echo ";;;;;; GERANDO ;;;;;;"
+echo ""
+
+echo -n "palavra : " >> md5.txt
+echo -n "$palavra" | md5sum | cut -d " " -f 1 >> md5.txt
+
+tail -n 1 md5.txt
+echo " "
+echo ";;;;;;;;;;;;;;;;;;;;
+echo "" 
